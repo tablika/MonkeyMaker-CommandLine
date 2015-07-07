d257750a793610ee570ca83a66d50d0fb75a3527
Peyman Mortazavi <pey.mortazavi@gmail.com>
2015-07-07 12:33:59 -0700

    Implemented config sub-command

--- src/config.js
+++ src/config.js
@@ -1,6 +1,14 @@
 var fs = require('fs-extra');
+var Table = require('cli-table');   // That nice table!
 
 module.exports.desc = "View or update the build config file.";
+module.exports.action = function(cmd) {
+  try {
+    processFlags(cmd);
+  } catch(exception){
+    console.error('Could not find/load monkey.json');
+  }
+}
 module.exports.commands = {};
 
 module.exports.commands.set = {
@@ -15,11 +23,58 @@ module.exports.commands.set = {
       var monkeyConfig = JSON.parse(fs.readFileSync('monkey.json', 'utf8'));
       setValue(monkeyConfig, keyPath, value);
       fs.writeFileSync('monkey.json', JSON.stringify(monkeyConfig, null, 2));
+      processFlags(cmd);
     } catch(exception) {
       console.error('Could not find/load monkey.json');
     }
 
   }
+}
+
+module.exports.commands.get= {
+  argsCount: 1,
+  desc: "get value of a given key path.",
+  action: function(cmd) {
+    try{
+      var monkeyConfig = JSON.parse(fs.readFileSync('monkey.json', 'utf8'));
+      var keyValuePair = flatten(monkeyConfig);
+      if(keyValuePair[cmd.args[0]]) console.log(keyValuePair[cmd.args[0]]);
+      else console.error('No value is set to key path: ' + cmd.args[0]);
+    } catch(exception) {
+      console.error('Could not find/load monkey.json');
+    }
+  }
+}
+
+function processFlags(cmd) {
+  var argv = cmd.argv;
+  if(argv.list) {
+    var table = new Table({ chars: { 'top': '' , 'top-mid': '' , 'top-left': '' , 'top-right': ''
+      , 'bottom': '' , 'bottom-mid': '' , 'bottom-left': '' , 'bottom-right': ''
+      , 'left': '~' , 'left-mid': '' , 'mid': '' , 'mid-mid': ''
+      , 'right': '' , 'right-mid': '' , 'middle': ':' }, style: { }});
+
+    var monkeyConfig = JSON.parse(fs.readFileSync('monkey.json', 'utf8'));
+    var keyValuePair = flatten(monkeyConfig);
+    for(key in keyValuePair) {
+      table.push([key, keyValuePair[key]]);
+    }
+    console.log(table.toString());
+  }
+}
+
+function flatten(object, keyValuePair, prefix) {
+  keyValuePair = keyValuePair || {};
+  for (var key in object) {
+    if (object.hasOwnProperty(key)) {
+      if(typeof(object[key]) == 'object') {
+        flatten(object[key], keyValuePair, prefix?prefix+'.'+key:key);
+      } else {
+        keyValuePair[prefix?prefix+'.'+key:key] = object[key];
+      }
+    }
+  }
+  return keyValuePair;
 }
 
 function setValue(object, keyPath, value) {
