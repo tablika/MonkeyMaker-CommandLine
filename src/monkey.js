17ca8bbca76f8856c1a25508376f62381c455548
Peyman Mortazavi <pey.mortazavi@gmail.com>
2015-07-21 20:49:43 -0700

    increased version

--- src/monkey.js
+++ src/monkey.js
@@ -6,22 +6,7 @@ var commands = {
   deploy: require('./deploy.js'),
   init: require('./init.js'),
   install: require('./install.js'),
-  serve: require('./serve/serve.js'),
-  test: {
-    action: function() {
-      var spawn = require('child_process').spawn;
-      var shellSyntaxCommand = '"export DELIVER_USER="{0}" && export DELIVER_PASSWORD="{1}" && deliver testflight -f {2} >> a.txt"'.format('app@tablika.com', 'SMhereitis@123', '/Users/peyman/Projects/Tablika/src/output/tablika.prod/ios/app.ipa');
-      console.log(shellSyntaxCommand);
-      var exec = require('child_process').execSync;
-      var results = exec('export DELIVER_USER="{0}" && export DELIVER_PASSWORD="{1}" && deliver testflight -f {2} >> a.txt'.format('app@tablika.com', 'SMhereitis@123', '/Users/peyman/Projects/Tablika/src/output/tablika.prod/ios/app.ipa'))
-      console.log(results);
-      // var a = spawn('sh', ['-c', shellSyntaxCommand], { detached: false }, function(err){
-      //   console.log("HI");
-      //   console.log(err);
-      // });
-      // console.log(a.pid);
-    }
-  }
+  serve: require('./serve/serve.js')
 }
 
 cmdliner.init(commands);
